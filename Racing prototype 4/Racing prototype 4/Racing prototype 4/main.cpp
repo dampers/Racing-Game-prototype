5bccb9c60dc4e7a921e4fb7b410160dfbae4580f
dampers <houma757@gmail.com>
2021-08-16 21:20:13 +0900

    map class

--- Racing prototype 4/Racing prototype 4/Racing prototype 4/main.cpp
+++ Racing prototype 4/Racing prototype 4/Racing prototype 4/main.cpp
@@ -5,7 +5,7 @@
 #include <windows.graphics.h>
 
 #pragma warning(disable:4996)
-
+#define PI 3.1415926535
 
 const int CAMERA_WIDTH = 80;
 const int CAMERA_HEIGHT = 25;
@@ -14,7 +14,6 @@ const int MAP_HEIGHT = 500;
 const int MAP_WIDTH = 800;
 const int SCREEN_HEIGHT = 25;
 const int SCREEN_WIDTH = 80;
-const double PI = 3.141592;
 
 enum Direction { STOP = 0, UP, DOWN, RIGHT, LEFT, DRIFT };
 
@@ -221,6 +220,38 @@ public:
 class Map
 {
     char game_map[1001][1001];
+
+    void SetMap()
+    {
+        memset(game_map, 0, sizeof(game_map));
+        for (int i = 0; i < MAP_HEIGHT; i++)
+        {
+            for (int j = 0; j < MAP_WIDTH; j++)
+            {
+                game_map[i][j] = ' ';
+            }
+        }
+        int bound_x = 2 * CAMERA_WIDTH, bound_y = 2 * CAMERA_HEIGHT;
+        for (int i = bound_x; i < MAP_WIDTH - bound_x; i++)
+            game_map[bound_y][i] = game_map[MAP_HEIGHT - bound_y][i] = '*';
+        for (int i = bound_y; i < MAP_HEIGHT - bound_y; i++)
+            game_map[i][bound_x] = game_map[i][MAP_WIDTH - bound_x] = '*';
+
+        int sq_x = MAP_WIDTH / 3, sq_y = MAP_HEIGHT / 4;
+        for (int i = sq_y; i < sq_y + sq_y; i++)
+        {
+            for (int j = sq_x; j < sq_x + sq_x; j++)
+            {
+                game_map[i][j] = 'G';
+            }
+        }
+        for (int i = CAMERA_WIDTH << 1; i < sq_x; i++)
+            game_map[MAP_HEIGHT >> 1][i] = '-';
+    }
+    void GetMap(FILE* map_file)
+    {
+
+    }
 
 };
 
